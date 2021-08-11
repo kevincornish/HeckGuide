d0cabbb0a6d1fea1fe84bbf1b854f6a1e7410b3d
Kev <kevinz_c@live.com>
2021-08-11 10:02:56 +0100

    implement search via component name

--- world/views.py
+++ world/views.py
@@ -12,11 +12,14 @@ class WorldListView(LoginRequiredMixin, ListView):
         player = self.request.GET.get('player')
         clan = self.request.GET.get('clan')
         realm = self.request.GET.get('realm')
+        name = self.request.GET.get('name')
         object_list = (
             WorldSegments.objects.all().order_by('-x')
         )
         if player:
             object_list = object_list.filter(owner_username__iexact=player)
+        if name:
+            object_list = object_list.filter(name__iexact=name)
         if clan:
             object_list = object_list.filter(owner_group_name__iexact=clan)
         if realm:
@@ -26,6 +29,7 @@ class WorldListView(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
         object_list = (WorldSegments.objects.all())
+        data['names'] = object_list.distinct('name').exclude(owner_user_id__isnull=False)
         data['clans'] = object_list.distinct('owner_group_name').exclude(owner_group_name__isnull=True)
         data['realms'] = object_list.distinct('world_id')
         return data

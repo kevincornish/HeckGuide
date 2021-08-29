0f48298a451df230dffdc48cd566737248e98808
Kev <kevinz_c@live.com>
2021-08-29 10:08:23 +0100

    added realm maps as submenu

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -22,8 +22,16 @@ $('#calc-expand-link').click( function() {
   $(".mob-sub").toggleClass("mob-sub-show");
 } );
 
+$('#realm-expand-link').click( function() {
+  $(".mob-sub-realm").toggleClass("mob-sub-show");
+} );
+
 $('#main-sub-link, .main-nav-sub').hover( function() {
   $(".main-nav-sub").toggleClass("ms-show");
+} );
+
+$('#realm-sub-link, .realm-nav-sub').hover( function() {
+  $(".realm-nav-sub").toggleClass("ms-show");
 } );
 
 $('#account-btn, .main-account-sub').hover( function() {

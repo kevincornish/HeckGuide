d7fec0280e11e333d88703d58016bde92d9d83d8
Kev <kevinz_c@live.com>
2021-09-11 14:58:50 +0100

    realm106 support

--- allies/management/commands/strip_allies.py
+++ allies/management/commands/strip_allies.py
@@ -19,8 +19,8 @@ class Command(BaseCommand):
         staytoken = settings.STAY_ALIVE_TOKEN
         if options['token'] == 1:
             token = settings.HECKFIRE_API_TOKEN
-        elif options['token'] == 23:
-            token = settings.TOKEN_23
+        elif options['token'] == 106:
+            token = settings.TOKEN_106
         elif options['token'] == 10:
             token = settings.TOKEN_10
         elif options['token'] == 128:
--- world/management/commands/crawl_world.py
+++ world/management/commands/crawl_world.py
@@ -19,8 +19,8 @@ class Command(BaseCommand):
             token = settings.HECKFIRE_API_TOKEN
         elif options['token'] == 128:
             token = settings.TOKEN_128
-        elif options['token'] == 23:
-            token = settings.TOKEN_23
+        elif options['token'] == 106:
+            token = settings.TOKEN_106
         elif options['token'] == 10:
             token = settings.TOKEN_10
         elif options['token'] == 129:

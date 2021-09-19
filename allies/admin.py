5b4d47071c5bacb9ff2c2a2a1bb76df21a522610
Kev <kevinz_c@live.com>
2021-09-19 19:26:45 +0100

    added ally & world admin

--- allies/admin.py
+++ allies/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Ally, HistoricalAlly
 
-# Register your models here.
+class AllyAdmin(admin.ModelAdmin):
+  list_display = ('username', 'created', 'last_modified')
+
+class HistoricalAllyAdmin(admin.ModelAdmin):
+  list_display = ('username', 'user_id', 'created', 'last_modified')
+  search_fields = ['username', 'user_id']
+
+admin.site.register(Ally, AllyAdmin)
+admin.site.register(HistoricalAlly, HistoricalAllyAdmin)
--- world/admin.py
+++ world/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import WorldSegments
 
-# Register your models here.
+class WorldSegmentsAdmin(admin.ModelAdmin):
+  list_display = ('description', 'name', 'owner_group_name', 'owner_username', 'world_id', 'created', 'last_modified')
+  search_fields = ['owner_username']
+
+admin.site.register(WorldSegments, WorldSegmentsAdmin)

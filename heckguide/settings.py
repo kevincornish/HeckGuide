e8d78aee175aaef0d242ab252bf255ec3c8646bd
Kev <kevinz_c@live.com>
2021-09-23 09:54:38 +0100

    added django debug toolbar

--- heckguide/settings.py
+++ heckguide/settings.py
@@ -171,6 +171,14 @@ STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
 if DEBUG:
 	EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+	# django-debug-toolbar
+	INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
+	MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
+	DEBUG_TOOLBAR_CONFIG = {
+        "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
+        "SHOW_TEMPLATE_CONTEXT": True,
+    }
+	INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
 
 if not DEBUG:
 	STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

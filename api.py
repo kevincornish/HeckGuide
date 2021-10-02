cdefc66cc300edd63161e2fbd86b2808bf7cd989
Kev <kevinz_c@live.com>
2021-10-02 12:47:31 +0100

    Unhide Timer Hide all required login urls Tidy realm list to 3x3 grid Add clan join request api function Add comments to api functions Fix Chat and Clan table css headers Add Clan lists to URLs

--- api.py
+++ api.py
@@ -53,6 +53,7 @@ class HeckfireApi(object):
             raise TokenException(f"Failed to fetch token from heck api. Error: {message}")
 
     def stay_alive(self):
+        """Sends the first and second half of each token to the ticket response to keep the token alive."""
         data = {"authorization": f"Session {self.staytoken}:{self.token}", "Accept": "application/json"}
         url = f"{self.base_url}/support/tickets/"
         req = requests.get(url, headers=data)
@@ -62,6 +63,7 @@ class HeckfireApi(object):
         return json_data
 
     def collect_loot(self):
+        """Collects any sold ally gold from the treasury."""
         data = {"authorization": f"Bearer {self.token}", "Accept": "application/json"}
         url = f"{self.base_url}/game/resource/collect_unlootable_resources/"
         req = requests.get(url, headers=data)
@@ -71,31 +73,38 @@ class HeckfireApi(object):
         return json_data
 
     def get_user_by_name(self, username: str) -> Dict:
+        """Searches the users api using username."""
         url = f"{self.base_url}/game/user/search_by_name/"
         data = {"username": username}
         return self._post(url, data)
 
     def get_ally_by_name(self, username: str) -> Dict:
+        """Searches the ally api using username."""
         url = f"{self.base_url}/game/ally/search_allies_by_username/"
         data = {"ally_username": username}
         return self._post(url, data)
 
     def get_clan_by_id(self, group_id: int) -> Dict:
+        """Searches the clan api for a group with given group_id."""
         url = f"{self.base_url}/game/group/get_group/"
         data = {"group_id": group_id}
         return self._post(url, data)
 	
     def get_allies_by_price(self, price: int, offset: int = 0) -> Dict:
+        """Searches the ally api using max cost and page offset."""
         url = f"{self.base_url}/game/ally/search_allies"
         data = {'max_cost': price, 'offset': offset}
         return self._post(url, data)
 
     def buy_ally(self, username: str, cost: int) -> Dict:
+        """Purchases an ally by ID and expected cost to the current logged in account."""
         url = f"{self.base_url}/game/ally/buy_ally"
         data = {'ally_user_id': username, 'expected_cost': cost}
         return self._post(url, data)
 
     def poll_chat(self):
+        """Polls the logged in users global/clan and announcement chats.
+        Currently only returning the global chat."""
         data = {"authorization": f"Bearer {self.token}", "Accept": "application/json"}
         url = f"{self.base_url}/game/poll/chat"
         req = requests.get(url, headers=data)
@@ -106,6 +115,7 @@ class HeckfireApi(object):
         return chats
 
     def poll_realm_list(self):
+        """Polls the list of active realms usercounts, names and descriptions, returns the shards response only"""
         data = {"authorization": f"Bearer {self.token}", "Accept": "application/json"}
         url = f"{self.base_url}/game/shard/get_transferable_shards/"
         req = requests.get(url, headers=data)
@@ -116,6 +126,7 @@ class HeckfireApi(object):
         return realms
 
     def poll_group_power_leaderboard(self):
+        """Polls the clans top might/power leaderboards for given tokens realm"""
         data = {"authorization": f"Bearer {self.token}", "Accept": "application/json"}
         url = f"{self.base_url}/game/leaderboard/get_group_power_leaderboard"
         req = requests.get(url, headers=data)
@@ -126,6 +137,7 @@ class HeckfireApi(object):
         return group_power_leaderboard
         
     def poll_group_troopkill_leaderboard(self):
+        """Polls the clans top troop kills leaderboards for given tokens realm"""
         data = {"authorization": f"Bearer {self.token}", "Accept": "application/json"}
         url = f"{self.base_url}/game/leaderboard/get_group_troopkill_leaderboard"
         req = requests.get(url, headers=data)
@@ -136,6 +148,7 @@ class HeckfireApi(object):
         return group_troopkill_leaderboard
 
     def poll_user_power_leaderboard(self):
+        """Polls the user top might/power leaderboards for given tokens realm"""
         data = {"authorization": f"Bearer {self.token}", "Accept": "application/json"}
         url = f"{self.base_url}/game/leaderboard/get_user_power_leaderboard"
         req = requests.get(url, headers=data)
@@ -146,6 +159,7 @@ class HeckfireApi(object):
         return user_power_leaderboard
 
     def poll_user_troopkill_leaderboard(self):
+        """Polls the user troop kills leaderboards for given tokens realm"""
         data = {"authorization": f"Bearer {self.token}", "Accept": "application/json"}
         url = f"{self.base_url}/game/leaderboard/get_user_troopkill_leaderboard"
         req = requests.get(url, headers=data)
@@ -155,6 +169,17 @@ class HeckfireApi(object):
             raise TokenException(json_data['exception'])
         return user_troopkill_leaderboard
 
+    def poll_mail(self):
+        """Polls the logged in users mail."""
+        data = {"authorization": f"Bearer {self.token}", "Accept": "application/json"}
+        url = f"{self.base_url}/game/poll/mail"
+        req = requests.get(url, headers=data)
+        json_data = json.loads(req.text)
+        mails = json_data['mails']
+        if json_data.get('exception'):
+            raise TokenException(json_data['exception'])
+        return mails
+
     def _post(self, url: str, data: Dict) -> Dict:
         response = requests.post(url, headers=self.headers, data=data)
         json_data = json.loads(response.text)
@@ -163,6 +188,8 @@ class HeckfireApi(object):
         return json_data
 
     def fetch_world(self, lowerbound: int):
+        """Fetches 20 chunks of world map data only taking the 'sites' chunk of response.
+        20 is the api limit."""
         tiles = []
         url = f"{self.base_url}/game/nonessential/poll_segments_realm_state"
         data = {"segment_ids": [i for i in range(lowerbound, lowerbound + 20)]}
@@ -175,6 +202,7 @@ class HeckfireApi(object):
         return tiles
 
     def get_clan_for_user(self):
+        """Sends request to api to grab the current clan for logged in user, pulls group_id from response"""
         data = {"authorization": f"Bearer {self.token}", "Accept": "application/json"}
         url = f"{self.base_url}/game/group/get_group_for_user/"
         req = requests.get(url, headers=data)
@@ -184,7 +212,16 @@ class HeckfireApi(object):
             raise TokenException(json_data['exception'])
         return group_id
 
+    def get_clan_requests(self):
+        """Sends request to api to grab the requests to join the current clan for logged in user.
+        Requires High enough rank in clan or not authorized exception"""
+        group_id = self.get_clan_for_user()
+        url = f"{self.base_url}/game/group/get_join_requests/"
+        data = {"group_id": group_id}
+        return self._post(url, data)
+
     def message_clan(self, message: str) -> Dict:
+        """Sends given message to current tokens clan chat"""
         group_id = self.get_clan_for_user()
         url = f"{self.base_url}/game/message/send_group_chat/"
         data = {"group_id": group_id, "message": message}

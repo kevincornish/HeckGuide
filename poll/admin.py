972fc5d9832626878d99dd029485b1dc99639827
Kev <kevinz_c@live.com>
2021-10-04 09:18:25 +0100

    add clan chats to admin panel

--- poll/admin.py
+++ poll/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ClanChat
 
-# Register your models here.
+class ClanChatAdmin(admin.ModelAdmin):
+  list_display = ('mail_id', 'username', 'message', 'realm')
+
+admin.site.register(ClanChat, ClanChatAdmin)
